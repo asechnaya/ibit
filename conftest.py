e5277a3a64a4c6192b920aa35bf262bb891c47bc
amakarovskaya <anutya@mail.ru>
2022-12-16 18:27:03 +0400

    headless opt for FF

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FFOptions
 from logs.testlogger import logger
 from pages.base_page import BasePage
 from pages.global_variables import LINK, PASSWORD, PREFIX, USER
@@ -14,8 +15,8 @@ def headless_chrome():
 
 
 def headless_firefox():
-    ops = webdriver.FirefoxOptions()
-    ops.headless = True
+    ops = FFOptions()
+    ops.add_argument("--headless")
     driver = webdriver.Firefox(options=ops)
     return driver
 
